554b2608db2b9aac94b3382c475a5f970878ecad
Ross Baker <rossbaker32@gmail.com>
2020-04-21 09:41:27 +1000

    added migration for beverages

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_054027) do
+ActiveRecord::Schema.define(version: 2020_04_20_234003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beverages", force: :cascade do |t|
+    t.string "name"
+    t.float "calories"
+    t.boolean "is_alcoholic"
+  end
 
   create_table "food_items", force: :cascade do |t|
     t.string "name"
